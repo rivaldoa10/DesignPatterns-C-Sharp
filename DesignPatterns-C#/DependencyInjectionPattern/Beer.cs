aa3b38bae58ddba1d8830fcf984b30d5cf3632df
rivaldoa10 <rivaldoaperez10@hotmail.com>
2026-04-16 16:38:41 -0600

    Dependency Injection

---
+++ DesignPatterns-C#/DependencyInjectionPattern/Beer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_C_.DependencyInjectionPattern
+{
+    public class Beer
+    {
+        private string _name;
+        private string _brand;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public Beer(string name, string brand, int a)
+        {
+            _name = name;
+            _brand = brand;
+        }
+    }
+}
--- DesignPatterns-C#/Program.cs
+++ DesignPatterns-C#/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatterns_C_.FactoryPattern;
+﻿using DesignPatterns_C_.DependencyInjectionPattern;
+using DesignPatterns_C_.FactoryPattern;
 using DesignPatterns_C_.Singleton;
 
 //Singleton
@@ -9,13 +10,18 @@ using DesignPatterns_C_.Singleton;
 //log.Save("Test 2");
 
 //Factory
-SaleFactory storeSaleFactory = new StoreSaleFactory(10);
-SaleFactory internetSaleFactory = new InternetSaleFactory(10);
+//SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+//SaleFactory internetSaleFactory = new InternetSaleFactory(10);
 
-ISale sale1 = storeSaleFactory.GetSale();
-sale1.Sell(15);
+//ISale sale1 = storeSaleFactory.GetSale();
+//sale1.Sell(15);
 
-ISale sale2 = internetSaleFactory.GetSale();
-sale2.Sell(15);
+//ISale sale2 = internetSaleFactory.GetSale();
+//sale2.Sell(15
+
+//Dependency Injection
+var beer = new Beer("Toña", "Toña", 1);
+var drinkWithBeer = new DrinkWithBeer(10, 1, beer);
+drinkWithBeer.Build();
 
 
